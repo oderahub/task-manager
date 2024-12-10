712ff0b68ddb81a648a87f06e396e885eae0171c
oderahub <okere_chidera@yahoo.com>
2024-12-10 09:09:29 +0100

    feat: users model and controller

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -5,15 +5,48 @@ const asyncWrapper = require("../middleware/asyncWrapper")
 
 
 
-const createUser = async (req, res, next) => {
-
+const registerUser = asyncWrapper(async (req, res) => {
     const { email, password, username } = req.body
 
-    if (!email && !password) {
-        throw createCustomError("Email and Password must be provided", 404)
+    if (!email || !password || !username) {
+        throw createCustomError("User must provide email, password and username", 400)
     }
+    const existingEmail = await User.findOne({ email })
 
+    if (existingEmail) {
+        throw createCustomError("User email exist, login", 409)
+    }
+    const newUser = new User({ email, password, username })
+    await newUser.save()
+    res.status(201).json({
+        message: "User registered", newUser
+    })
 
 }
+)
 
 
+const loginUser = asyncWrapper(async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw createCustomError("Invaild email or password", 400)
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw createCustomError("Invaild email or password", 401)
+    }
+
+    const validatePassword = await user.comparePassword(password)
+
+    if (!validatePassword) {
+        throw createCustomError("Invalid email or password", 404)
+    }
+
+    res.status(200).json({ message: "login successful" })
+
+}
+)
+
+
+module.exports = { registerUser, loginUser }
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 
 const mongoose = require("mongoose")
 const { v4: uuidv4 } = require("uuid")
+const bcrypt = require("bcrypt")
 
 
 const UserShema = new mongoose.Schema({
@@ -15,11 +16,31 @@ const UserShema = new mongoose.Schema({
     },
     username: {
         type: String,
+        set: (value) => value.toLowerCase()
 
     },
     password: {
         type: String
     }
 })
+
+UserShema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        return next()
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+    } catch (error) {
+        next(error)
+    }
+})
+
+UserShema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password)
+
+}
 
 module.exports = mongoose.model("User", UserShema)
